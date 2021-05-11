d2b272106bdc565eb41488d1e9badc950ebac27a
Vinicius Miranda <v-alves-miranda@live.com>
2021-05-11 17:54:10 -0300

    for encadeado

---
+++ AprendendoCSharp/P13-ForEncadeado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa 13 - For Encadeado");
+            Console.WriteLine();
+            for (int linha = 0; linha<=10; linha++)
+            {
+                for(int coluna = 0; coluna<=10; coluna++)
+                {
+                    Console.Write("*");
+                    if (coluna >= linha)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("O programa finalizou. tecle ENTER para encerrar...");
+            Console.ReadLine();
+        }
+    }
+}
