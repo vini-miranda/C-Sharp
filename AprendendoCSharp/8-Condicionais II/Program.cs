0e9ff0794f02afc790dd9e3c2eca7d200aa9b426
Vinicius Miranda <v-alves-miranda@live.com>
2021-05-11 16:09:10 -0300

    Condicionais e escopo

---
+++ AprendendoCSharp/8-Condicionais II/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _8_Condicionais_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa 8 - Condicionais II");
+
+            int idade = 16;
+            bool acompanhado = true;
+            Console.WriteLine("Sua idade: " + idade);
+            Console.WriteLine("Acompanhado?: " + acompanhado);
+
+            if (idade >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+
+            }
+
+            Console.WriteLine("O programa finalizou. tecle ENTER para encerrar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AprendendoCSharp/9-Escopo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa 9 - Escopo");
+
+            int idade = 16;
+            bool acompanhado = false;
+            string mensagem;
+
+            if (acompanhado==true)
+            {
+                mensagem = "Sim.";
+            }
+            else
+            {
+                mensagem = "Não.";
+            }
+
+            Console.WriteLine("Sua idade: " + idade);
+            Console.WriteLine("Acompanhado? " + mensagem);
+
+            if (idade >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+
+            }
+
+            Console.WriteLine("O programa finalizou. tecle ENTER para encerrar...");
+            Console.ReadLine();
+        }
+    }
+}
