e65630ab4393fa8eed689ecf08be78ece3fddb92
Vinicius Miranda <v-alves-miranda@live.com>
2021-05-12 11:36:13 -0300

    iniciando POO

---
+++ Orientação a Objetos - ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Gabriela";
+            conta.agencia = 356;
+            conta.conta = 356741;
+            conta.saldo = 100.75;
+
+            Console.WriteLine("Nome: " + conta.titular);
+            Console.WriteLine("Agência: " + conta.agencia);
+            Console.WriteLine("Conta: " + conta.conta);
+            Console.WriteLine("Saldo: R$" + conta.saldo);
+        }
+    }
+}
---
+++ Orientação a Objetos - ByteBank/02-ValorEReferencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02_ValorEReferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+            ContaCorrente conta2 = new ContaCorrente();
+            conta1.saldo = 200;
+            conta1.saldo += 100;
+            conta2.saldo = 50;
+
+            Console.WriteLine("A conta 1 possui: R$" + conta1.saldo);
+            Console.WriteLine("A conta 2 possui: R$" + conta2.saldo);
+
+
+        }
+    }
+}
